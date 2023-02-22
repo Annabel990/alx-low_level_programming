a28df3948762e6fe1eb5be78664bc39764d13ff4
Annabel990 <Annabel990>
2023-02-22 05:18:51 -0800

    5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign-
+ * @n
+ * Description
+ * Return:
+ */
+int print_sign(int n)
+{
+        if (n > 0)
+		_putchar(+);
+                return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	else (n < 0)
+		_putchar(-);
+		return (-1);
+}
+      
