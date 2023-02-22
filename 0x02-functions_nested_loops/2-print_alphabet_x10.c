6e703cb6f62365682da50f7273997a40b5d18835
Annabel990 <Annabel990>
2023-02-21 17:30:16 -0800

    prints the alphabet, print 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char alph;
+	char c;
 	int i = 0;
 
 	while (i <= 9)
 	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
 
-	for (alph = 'a'; alph <= 'z'; alph++)
-	{
-		putchar(alph);
-	}
-       	putchar('\n');
-	      i++;
+		putchar('\n');
+
+		i++;
 	}
 }
