576385c706e0b9711c142b4e76eb17eaf678fef4
Annabel990 <Annabel990>
2023-02-28 15:01:15 -0800

    strlen

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,12 +5,11 @@
  *
  * Return: Always 0.
  */
-
-void _puts(char *str)
+int main(void)
 {
-    char *str;
+	char *str;
 
-    str = "I do not fear computers. I fear the lack of them - annabel";
-    _puts(str);
-    return (0);
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
 }
