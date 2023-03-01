ab6d39f4dc0d0053f46cfddd04b241c221b8bd69
Annabel990 <Annabel990>
2023-03-01 15:45:08 -0800

    rev-array

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,10 +5,16 @@
  * @s: string to manipulate
  * Return: string with all letters capitalized
  */
+
 char *string_toupper(char *s)
 {
-        while (*s) //if s exists will omit null terminator
-	    if (*s >= 'a' && *s <= 'z')
-	        *s = *s -'a' + 'A';
-	    s++;
+
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
 }
