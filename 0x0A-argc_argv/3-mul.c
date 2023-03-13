785ca873414547152ffb5ae9879134d5fc4e47a8
Annabel990 <Annabel990>
2023-03-13 13:12:33 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 3)
+
+	/* validate input */
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
+	/* mulitply two arguments passed via cmd line */
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-		
+}
