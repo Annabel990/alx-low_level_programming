5038b311d2ae73acb96739d77e60cf3df36bad79
Annabel990 <Annabel990>
2023-03-13 13:24:13 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - print each argument passed to program on a line
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i = 0;
